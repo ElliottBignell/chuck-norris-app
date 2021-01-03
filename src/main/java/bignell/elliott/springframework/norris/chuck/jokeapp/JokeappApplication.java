1c9e74b168dae373cf01932eab2ab7aec4f4e144
Elliott Bignell <elliott.bignell@gmail.com>
2021-01-03 16:21:05 +0100

    Initial commit

---
+++ src/main/java/bignell/elliott/springframework/norris/chuck/jokeapp/JokeappApplication.java
@@ -0,0 +1,13 @@
+package bignell.elliott.springframework.norris.chuck.jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bignell/elliott/springframework/norris/chuck/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package bignell.elliott.springframework.norris.chuck.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
